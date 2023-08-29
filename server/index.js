8e71d9171fd2712995c11c37d9433e315f754a7e
NikitaTarasenko <36858449+NikitaTarasenko@users.noreply.github.com>
2023-08-29 15:25:29 +0300

    12

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const app = express();
 const WSServer = require("express-ws")(app);
 const aWss = WSServer.getWss();
 const cors = require("cors");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const fs = require("fs");
 const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.ws("/", (ws, req) => {
   ws.on("message", (msg) => {
@@ -36,6 +37,7 @@ app.post("/image", (req, res) => {
 });
 
 app.get("/image", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
   try {
     const file = fs.readFileSync(path.resolve(__dirname, "files", `${req.query.id}.jpg`));
     const data = `data:image/png;base64,` + file.toString("base64");
